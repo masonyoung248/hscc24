dbcb8acf8370124d3dce40da65239cf1e1214988
kempensp <kempensp@milwaukee.k12.wi.us>
2024-02-10 18:58:47 +0000

    Started form

--- inbdpa24/app.js
+++ inbdpa24/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/page2', page2Router);
+app.use('/index', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- inbdpa24/routes/page2.js
+++ inbdpa24/routes/page2.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BDPA Page 2' });
+  res.render('page2', { title: 'BDPA Page 2' });
 });
 
 module.exports = router;
